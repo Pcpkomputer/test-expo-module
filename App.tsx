55e65d4775c6be6a5506fe25772dfab983248d9b
pcpkomputer <terrificsubs@gmail.com>
2022-12-28 22:12:22 +0700

    initial commit

---
+++ App.tsx
@@ -0,0 +1,6 @@
+import { WebView } from './modules/expo-web-view/src';
+import {Text} from "react-native";
+
+export default function App() {
+  return  <WebView style={{ flex: 1, backgroundColor: 'purple' }} />;
+}
